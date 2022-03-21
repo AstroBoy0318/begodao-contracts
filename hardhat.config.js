0fa03927796cab50c77bea71404760accd657765
toby0318 <78232000+toby0318@users.noreply.github.com>
2022-03-21 11:57:54 +0900

    added lottery contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -74,6 +74,14 @@ module.exports = {
                     },
                 },
             }, {
+                version: "0.8.7",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            }, {
                 version: "0.7.5",
                 settings: {
                     optimizer: {
--- scripts/deploy_mumbai_3.js
+++ scripts/deploy_mumbai_3.js
@@ -15,7 +15,7 @@ async function main() {
     var nonce = await provider.getTransactionCount(deployer.address);
     console.log(nonce);
     var startTIme = new Date().getTime();
-    var stakingStartTime = Math.round(startTIme / 1000);
+    /*var stakingStartTime = Math.round(startTIme / 1000);
 
     // Deploy Token
     const _xbego = await ethers.getContractFactory('xBegoToken');
@@ -30,11 +30,24 @@ async function main() {
     const _stakingPool = await ethers.getContractFactory('StakingPool');
     const stakingPool_1 = await _stakingPool.deploy(xbego.address, daiAddress, "10000000000000", stakingStartTime, deployer.address, { nonce: nonce++ });
     const stakingPool_2 = await _stakingPool.deploy(xbego.address, begoAddress, "10000000", stakingStartTime, deployer.address, { nonce: nonce++ });
+    */
+
+    var ReferralContract = await ethers.getContractFactory('Refferal');
+    var referralContract = await ReferralContract.deploy({ nonce: nonce++ });
+
+    var RandomGenerator = await ethers.getContractFactory('RandomGenerator');
+    var randomGenerator = await RandomGenerator.deploy(["0x007A22900a3B98143368Bd5906f8E17e9867581b", "0x0FCAa9c899EC5A91eBc3D5Dd869De833b06fB046", "0x0715A7794a1dc8e42615F059dD6e406A6594651A", "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada", "0x9dd18534b8f456557d11B9DDB14dA89b2e52e308", "0x572dDec9087154dC5dfBB1546Bb62713147e0Ab0", "0x92C09849638959196E976289418e5973CC96d645"], { nonce: nonce++ });
+
+    var LotteryContract = await ethers.getContractFactory('Lottery');
+    var lotteryContract = await LotteryContract.deploy("0xbf739F95bF8A5E8a9f8Ae0778aA07f872f91c0A3", randomGenerator.address, { nonce: nonce++ });
 
     var end = new Date().getTime();
 
     console.log("deploy ended ", (Number(end) - startTIme) / 1000);
-    console.log("XBEGO_ADDRESS: ", xbego.address);
+    console.log("referral: ", referralContract.address);
+    console.log("randomGenerator: ", randomGenerator.address);
+    console.log("Lotter: ", lotteryContract.address);
+    /*console.log("XBEGO_ADDRESS: ", xbego.address);
     console.log("MASTERCHEF_ADDRESS: ", masterchef.address);
     console.log("STAKING_POOL_1: ", stakingPool_1.address);
     console.log("STAKING_POOL_2: ", stakingPool_2.address);
@@ -48,7 +61,7 @@ async function main() {
         constructorArguments: [
             xbego.address, stakingStartTime, deployer.address
         ],
-    });
+    });*/
 }
 
 main()
